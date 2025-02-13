5ea5ebde5b599b7cbd225f2e371e7c37f9d567e3
austinmula <maustin64.ma@gmail.com>
2025-02-13 13:17:00 +0300

    integrating category get groq method and types

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,21 +1,19 @@
 // import axios from 'axios';
 
-import { Category } from '@/models/category';
-import sanityClient from './sanity';
+import { Category } from "@/models/category";
+import sanityClient from "./sanity";
 // import { Game, GameSubset } from '@/models/game';
 
 export const getCategories = async (): Promise<Category[]> => {
-	const query = `*[_type == "category"] {
+  const query = `*[_type == "category"] {
         _id,
-        name,
-        slug {current},
+        slug,
         image, 
-        subtitle
+        title
     }`;
 
-	const categories: Category[] = await sanityClient.fetch({ query });
-
-	return categories;
+  const categories: Category[] = await sanityClient.fetch(query);
+  return categories;
 };
 
 // export const getGames = async (): Promise<Game[]> => {
