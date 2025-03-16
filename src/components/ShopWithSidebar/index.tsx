cb564bbb2e918c70391f4ecfea715c37b26f0faa
austinmula <maustin64.ma@gmail.com>
2025-03-16 17:07:50 +0300

    shop number of cols mod

--- src/components/ShopWithSidebar/index.tsx
+++ src/components/ShopWithSidebar/index.tsx
@@ -149,7 +149,7 @@ const ShopWithSidebar = () => {
         pages={["shop", "/", "product shop"]}
       />
       <section className="overflow-hidden relative pb-20 pt-5 lg:pt-20 xl:pt-28 bg-[#f3f4f6]">
-        <div className="max-w-[1170px] w-full mx-auto px-4 sm:px-8 xl:px-0">
+        <div className="max-w-[1440px] w-full mx-auto px-4 sm:px-8 xl:px-0">
           <div className="flex gap-7.5">
             {/* <!-- Sidebar Start --> */}
             <div
@@ -219,7 +219,7 @@ const ShopWithSidebar = () => {
             {/* // <!-- Sidebar End --> */}
 
             {/* // <!-- Content Start --> */}
-            <div className="xl:max-w-[870px] w-full">
+            <div className="xl:max-w-[1020px] w-full">
               <div className="rounded-lg bg-white shadow-1 pl-3 pr-2.5 py-2.5 mb-6">
                 <div className="flex items-center justify-between">
                   {/* <!-- top bar left --> */}
@@ -314,7 +314,7 @@ const ShopWithSidebar = () => {
               {/* <!-- Products Grid Tab Content Start --> */}
               <div
                 className={`${productStyle === "grid"
-                  ? "grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-x-7.5 gap-y-9"
+                  ? "grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-x-7.5 gap-y-9"
                   : "flex flex-col gap-7.5"
                   }`}
               >
