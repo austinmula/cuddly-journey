8c6fd95a4c61f1911dfc04e48c8345bae5a2a5a8
Austin Mula <53308954+austinmula@users.noreply.github.com>
2025-04-07 08:00:45 +0000

    url for utility function to enable null values without throwing error

--- src/lib/urlFor.ts
+++ src/lib/urlFor.ts
@@ -3,6 +3,14 @@ import sanityClient from "./sanity"; // Import the configured Sanity client
 
 const builder = imageUrlBuilder(sanityClient);
 
+// export function urlFor(source: any) {
+//   return builder.image(source)?.url() || "";
+// }
+
 export function urlFor(source: any) {
-  return builder.image(source).url();
+  if (!source) {
+    console.warn("Missing image source:", source);
+    return "";
+  }
+  return builder.image(source).url() || "";
 }
